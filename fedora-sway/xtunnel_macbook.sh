c3ac1a43fa40322ceefdb278c66234d13da0fd2e
Patrick Saguinsin <patrick.saguinsin@globe.com.ph>
2026-05-15 10:20:07 +0800

    Add exit on the ssh command to immediately exit on SIGINT

--- fedora-sway/xtunnel_macbook.sh
+++ fedora-sway/xtunnel_macbook.sh
@@ -3,4 +3,4 @@
 # ssh -ND8080 -c chacha20-poly1305@openssh.com a10017780@192.168.1.215 &
 #autossh -M 0 -N -D 8080 -c chacha20-poly1305@openssh.com -o "ServerAliveInterval 30" -o "ServerAliveCountMax 300" a10017780@192.168.1.215
 #autossh -M 0 -N -D 8080 -L 5900:localhost:5900 -c chacha20-poly1305@openssh.com -o "ServerAliveInterval 30" -o "ServerAliveCountMax 300" a10017780@192.168.1.215
-autossh -M 0 -t -D 8080 -L 5900:localhost:5900 -c chacha20-poly1305@openssh.com -o "ServerAliveInterval 30" -o "ServerAliveCountMax 300" a10017780@192.168.1.215 "sudo ~/Desktop/scripts/disable-sleep.sh"
+autossh -M 0 -t -D 8080 -L 5900:localhost:5900 -c chacha20-poly1305@openssh.com -o "ServerAliveInterval 30" -o "ServerAliveCountMax 300" a10017780@192.168.1.215 "sudo ~/Desktop/scripts/disable-sleep.sh ; exit"
