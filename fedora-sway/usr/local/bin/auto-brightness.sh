2b46e023f5876a59d3ceccbb18a08d3173ce7328
Patrick Saguinsin <patrick.saguinsin@globe.com.ph>
2026-06-20 13:29:49 +0800

    Add Fedora laptop auto-brightness on power state

---
+++ fedora-sway/usr/local/bin/auto-brightness.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Give the system a brief moment to register the hardware state change
+sleep 1
+
+if [ "$1" == "bat" ]; then
+    # Unplugged: dim the screen
+    /usr/bin/brightnessctl set 1
+elif [ "$1" == "ac" ]; then
+    # Plugged in: brighten the screen
+    /usr/bin/brightnessctl set 50%
+fi
